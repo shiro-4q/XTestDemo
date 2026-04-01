27f7cd87a89467518906abb475ea3fab710510ba
qw <qw@123.com>
2026-04-01 14:45:33 +0800

    添加项目文件。

---
+++ MyLibrary/MyCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary
+{
+    public class MyCalculator
+    {
+        public int Add(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
---
+++ MyLibraryTests/MyCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using MyLibrary;
+
+namespace MyLibraryTests
+{
+    public class MyCalculatorTests
+    {
+        [Fact]
+        public void Add_Test()
+        {
+            // Arrange
+            int i = 1;
+            int j = 2;
+            // Act
+            MyCalculator sut = new MyCalculator();
+            var actualResult = sut.Add(i, j);
+            // Assert
+            var expectedResult = 3;
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 4, 6)]
+        public void Add_TestV2(int i, int j, int expectedResult)
+        {
+            // Act
+            MyCalculator sut = new MyCalculator();
+            var actualResult = sut.Add(i, j);
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
