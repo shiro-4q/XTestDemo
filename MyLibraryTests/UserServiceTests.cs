4b050a2a441659b8e4c3c116c163740c113e1663
qw <qw@123.com>
2026-04-03 10:25:47 +0800

    新增 AutoFixture+AutoFixMoq+FluentAssertions Demo

--- MyLibraryTests/UserServiceTests.cs
+++ MyLibraryTests/UserServiceTests.cs
@@ -1,9 +1,13 @@
-﻿using MyLibrary;
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Moq;
+using MyLibrary;
 
 namespace MyLibraryTests
 {
     public class UserServiceTests
     {
+        // 不使用AutoFixture，手动创建UserRepositoryMock实例
         [Theory]
         [InlineData("abc@qq.com", "123456", "666666")]
         public async Task ChangeNewPassword_Test(string email, string oldPassword, string newPassword)
@@ -37,6 +41,19 @@ namespace MyLibraryTests
                 UpdateCallCount++;
                 return Task.FromResult(true);
             }
+        }
+
+        // 使用AutoFixture + AutoFixMoq 自动生成准备数据，自动注入实例
+        // 使用[Frozen]，确保注入的UserRepository实例是同一个
+        [Theory, AutoMoqData]
+        public async Task ChangeNewPassword_Test_Successfully([Frozen] Mock<IUserRepository> repoMock, UserService sut, int id, string email, string oldPassowrd)
+        {
+            var user = new User(id, email, oldPassowrd);
+            repoMock.Setup(r => r.GetByEmailAsync(email)).ReturnsAsync(user);
+            repoMock.Setup(r => r.UpdateAsync(user)).ReturnsAsync(true);
+            var actualResult = await sut.ChangeNewPasswordAsync(email, oldPassowrd, oldPassowrd + "_new");
+            actualResult.Should().BeTrue();
+            repoMock.Verify(r => r.UpdateAsync(It.IsAny<User>()), Times.Once);
         }
     }
 }
