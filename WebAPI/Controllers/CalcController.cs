ec863912ed6dccee4e01b1e6316b37475f30f953
qw_shrio <8599034+qw-shrio@user.noreply.gitee.com>
2026-04-06 17:42:16 +0800

    添加web api 集成测试示例，添加web api项目的stryker config

---
+++ WebAPI/Controllers/CalcController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        [HttpGet]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [HttpGet]
+        public int Reduce(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ WebAPITests/CalcControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace MyLibraryTests
+{
+    public class CalcControllerTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory = factory;
+
+        [Theory]
+        [InlineData("/api/calc/add", 1, 2, 3)]
+        [InlineData("/api/calc/add", -1, 1, 0)]
+        [InlineData("/api/calc/reduce", 3, 1, 2)]
+        [InlineData("/api/calc/reduce", 5, 2, 3)]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url, int paramA, int paramB, int expectedResult)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync($"{url}?a={paramA}&b={paramB}");
+            string actualResult = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();// Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+            Assert.Equal(expectedResult, int.Parse(actualResult));
+        }
+    }
+}
